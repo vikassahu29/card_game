be032f7872485a1407796d901113dcc390b20c35
Vikas Sahu <vikassahu2904@gmail.com>
2017-01-18 23:59:25 +0530

    WIP

--- lib/card_game/game.ex
+++ lib/card_game/game.ex
@@ -15,7 +15,13 @@ defmodule CardGame.Game do
     {"8", "Clubs"}, {"9", "Clubs"}, {"10", "Clubs"}, {"Jack", "Clubs"},
     {"Queen", "Clubs"}, {"King", "Clubs"}, {"Ace", "Clubs"}]
 
-  defstruct users: [], name: "", is_active: false, current_turn: "", user_cards: [], current_trick: [], user_tricks: []
+  @suits ["Diamonds", "Suits", "Clubs", "Hearts"]
+  @state_not_started 1
+  @state_select_trump 2
+  @state_trump_selected 3
+  @state_playing 4
+
+  defstruct users: [], name: "", state: @state_not_started, current_turn: "", user_cards: [], current_trick: [], last_trick: [], last_trick_winner: "" user_tricks: [], trump: ""
   def start_link(name, agent) do
     game = Agent.get(agent, fn x -> x end)
     GenServer.start_link(__MODULE__, {game, agent}, name: via_tuple(name))
@@ -45,6 +51,10 @@ defmodule CardGame.Game do
     GenServer.call(via_tuple(name), {:get_cards, user})
   end
 
+  def select_trump(name, user, card) do
+    GenServer.call(via_tuple(name), {:select_trump, user, card})
+  end
+
   def handle_cast({:leave, user}, {game, agent}) do
     users = game.users
     if (Enum.member?(users, user)) do
@@ -55,7 +65,15 @@ defmodule CardGame.Game do
     end
   end
 
-  def handle_call(:deal, _from, {game = %CardGame.Game{users: users, is_active: true}, agent}) do
+  def handle_call({:select_trump, user, suit}, _from, {game = %CardGame.Game{state: @state_select_trump, current_turn: user}, agent}) do
+    if (Enum.member?(@suits, suit)) do
+      {:reply, {:ok}, {%{game | state: @state_trump_selected, trump: suit}}}
+    else
+      {:reply, {:error, "Invalid Suit", {game, agent}}
+    end
+  end
+
+  def handle_call(:deal, _from, {game = %CardGame.Game{users: users, state: @state_trump_selected}, agent}) do
     if length(users) == 4 do
       new_game = %{deal_cards(game) | current_turn: hd(users)}
       {:reply, {:current_turn, new_game.current_turn}, {new_game, agent}}
@@ -63,18 +81,32 @@ defmodule CardGame.Game do
       {:reply, {:error, "Players not available"}, {game, agent}}
     end
   end
+#Todo
 
-  def handle_call({:play_turn, user, card}, _from, {game = %CardGame.Game{current_turn: user}, agent}) do
-    len = length(game.users)
-    next_user = Enum.at(game.users, rem(Enum.find_index(game.users, fn x -> x == user end) + 1, len))
-    {:reply, {user, card}, {%{game | current_turn: next_user}, agent}}
+  def handle_call({:play_turn, user, card}, _from, {game = %CardGame.Game{current_turn: user, current_trick: current_trick}, agent}) do
+    if (is_card_valid(card, current_trick, Enum.at(game.user_cards, [Enum.find_index(game.users, fn -> x == user end)]))) do
+      new_trick = [card | new_trick]
+      len = length(new_trick)
+      if (len === 4) do
+        new_game = calculate_result(%{game | current_trick: new_trick})
+        {:reply, {user, card}, {new_game, agent}}
+      else
+        next_user = Enum.at(game.users, rem(Enum.find_index(game.users, fn x -> x == user end) + 1, len))
+        {:reply, {user, card}, {%{game | current_turn: next_user }, agent}}
+      end
+    else
+      {:reply, {:error, "Invalid Card"}, game}
+    end
   end
 
   def handle_call({:join, user}, _from, {game, agent}) do
     users = game.users
     len = length(users)
     if len < 4 do
-      new_game = %{game | users: [ user | users], is_active: len == 3}
+      if (len == 3) {
+        state = @state_select_trump
+      }
+      new_game = %{game | users: [ user | users], state: state}
       {:reply, {:ok, new_game.users}, {new_game, agent}}
     else
       {:reply, {:error, "Server Full"}, {game, agent}}
@@ -96,7 +128,7 @@ defmodule CardGame.Game do
 
   defp deal_cards(game) do
     [head | tail] = game.users
-    deal_user(head, tail, game, Enum.shuffle(@cards))
+    deal_user(head, tail, g ame, Enum.shuffle(@cards))
   end
 
   defp deal_user(user, [], game, cards) do
@@ -107,6 +139,20 @@ defmodule CardGame.Game do
     new_game = %{game | user_cards: [ {user, Enum.take(cards, 13)} | game.user_cards]}
     rem_cards = Enum.drop(cards, 13)
     deal_user(x, xs, new_game, rem_cards)
+  end
+
+  defp is_card_valid(card, trick, user_cards) do
+    {_, suit} = Enum.reverse(trick)
+    {_, card_suit} = card
+    if (suit === card_suit) do
+      true
+    else
+      Enum.all?(user_cards, fn {_, s} -> s != suit end)
+    end
+  end
+
+  defp calculate_result(game) do
+
   end
 
 end
